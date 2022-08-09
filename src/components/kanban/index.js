fb522a17ac1eca117e7a16f1c06cc0e0cde0729a
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-08-08 21:45:26 -0300

    change kanban manage

--- src/components/kanban/index.js
+++ src/components/kanban/index.js
@@ -158,7 +158,7 @@ const Kanban = () => {
                                                                 opacity: snapshot.isDragging ? '0.5' : '1'
                                                             }}
                                                         >
-                                                            <Card.Text className='p-2 text-truncate card-width'>
+                                                            <Card.Text className='p-2 fs-6 justify-text hyphens'>
                                                                 <Button onClick={() => handleShowEdit(id, task, index)} className='me-3' size="sm" variant="outline-dark">
                                                                     <i className="bi bi-three-dots-vertical"></i>
                                                                 </Button>
